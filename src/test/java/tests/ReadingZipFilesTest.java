720fe040532dac9cb9e5e7bc31c41bbd02665139
alcarinquenessima <106439733+alcarinquenessima@users.noreply.github.com>
2024-08-11 12:42:47 +0300

    Update ReadingZipFilesTest.java

--- src/test/java/tests/ReadingZipFilesTest.java
+++ src/test/java/tests/ReadingZipFilesTest.java
@@ -11,7 +11,8 @@ import java.util.zip.ZipInputStream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ReadingZipFilesTest {
-    public void readPdfFromZip2(ZipInputStream zipInputStream) throws Exception {
+    
+    public void readPdfFromZipTest(ZipInputStream zipInputStream) throws Exception {
         PDF pdf = new PDF(zipInputStream);
         String actual = pdf.text;
         String expectedPdf1 = "Pdf file for testng";
@@ -19,7 +20,7 @@ public class ReadingZipFilesTest {
         assertThat(actual).contains(expectedPdf1);
     }
 
-    public void readExcelFromZip2(ZipInputStream zipInputStream) throws Exception {
+    public void readExcelFromZipTest(ZipInputStream zipInputStream) throws Exception {
         XLS xls = new XLS(zipInputStream);
         String firstUser = xls.excel.getSheetAt(0).getRow(0).getCell(0).toString();
         String firstUserXls = "dog1";
@@ -29,7 +30,7 @@ public class ReadingZipFilesTest {
         assertThat(firstUser).isEqualTo(firstUserXls);
         assertThat(secondUser).isEqualTo(secondUserXls);    }
 
-    public void readCsvFromZip2(ZipInputStream zipInputStream) throws Exception {
+    public void readCsvFromZipTest(ZipInputStream zipInputStream) throws Exception {
         CSVReader csvReader = new CSVReader(new InputStreamReader(zipInputStream));
 
         List<String[]> csvData = csvReader.readAll();
